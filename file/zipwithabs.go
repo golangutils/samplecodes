944fa2a446b46d11a2afcc306c890dbd95b77477
minnanalee <67048631+minnanalee@users.noreply.github.com>
2021-03-22 15:11:20 +0800

    Update zipwithabs.go

--- file/zipwithabs.go
+++ file/zipwithabs.go
@@ -1,95 +1,88 @@
-import (
-    "os"
-    "archive/zip"
-    "io"
-    "fmt"
-    "strings"
+
+packagemain
+
+import(
+"archive/zip"
+"fmt"
+"io"
+"os"
+"path/filepath"
+"strings"
 )
 
-// ZipFiles compresses one or many files into a single zip archive file.
+//ZipFilescompressesoneormanyfilesintoasingleziparchivefile.
 //压缩多个文件到一个文件里面
-// Param 1: 输出的zip文件的名字
-// Param 2: 需要添加到zip文件里面的文件
-//Param 3: 由于file是绝对路径，打包后可能不是想要的目录，oldform就是filename中需要被替换的掉的路径
-//Param 4: 要替换成的路径
-func ZipFiles(filename string, files []string, oldform, newform string) error {
+//Param1:输出的zip文件的名字
+//Param2:需要添加到zip文件里面的文件
+//Param3:由于file是绝对路径，打包后可能不是想要的目录，oldform就是filename中需要被替换的掉的路径
+//Param4:要替换成的路径
+funcZipFiles(filenamestring,files[]string,oldform,newformstring)error{
 
-    newZipFile, err := os.Create(filename)
-    if err != nil {
-        return err
-    }
-    defer newZipFile.Close()
+newZipFile,err:=os.Create(filename)
+iferr!=nil{
+returnerr
+}
+defernewZipFile.Close()
 
-    zipWriter := zip.NewWriter(newZipFile)
-    defer zipWriter.Close()
+zipWriter:=zip.NewWriter(newZipFile)
+deferzipWriter.Close()
 
-    // 把files添加到zip中
-    for _, file := range files {
+//把files添加到zip中
+for_,file:=rangefiles{
 
-        zipfile, err := os.Open(file)
-        if err != nil {
-            return err
-        }
-        defer zipfile.Close()
+zipfile,err:=os.Open(file)
+iferr!=nil{
+returnerr
+}
+deferzipfile.Close()
 
-        // 获取file的基础信息
-        info, err := zipfile.Stat()
-        if err != nil {
-            return err
-        }
+//获取file的基础信息
+info,err:=zipfile.Stat()
+iferr!=nil{
+returnerr
+}
 
-        header, err := zip.FileInfoHeader(info)
-        if err != nil {
-            return err
-        }
+header,err:=zip.FileInfoHeader(info)
+iferr!=nil{
+returnerr
+}
 
-        //使用上面的FileInforHeader() 就可以把文件保存的路径替换成我们自己想要的了，如下面
-        header.Name = strings.Replace(file, oldform, newform, -1)
+//使用上面的FileInforHeader()就可以把文件保存的路径替换成我们自己想要的了，如下面
+fmt.Println(file,oldform,newform)
+header.Name=strings.Replace(file,oldform,newform,-1)
 
-        // 优化压缩
-        // 更多参考see http://golang.org/pkg/archive/zip/#pkg-constants
-        header.Method = zip.Deflate
+//优化压缩
+//更多参考seehttp://golang.org/pkg/archive/zip/#pkg-constants
+header.Method=zip.Deflate
 
-        writer, err := zipWriter.CreateHeader(header)
-        if err != nil {
-            return err
-        }
-        if _, err = io.Copy(writer, zipfile); err != nil {
-            return err
-        }
-    }
-    return nil
+
+writer,err:=zipWriter.CreateHeader(header)
+iferr!=nil{
+returnerr
+}
+if_,err=io.Copy(writer,zipfile);err!=nil{
+returnerr
+}
+}
+returnnil
+}
+
+funcmain(){
+fileList:=[]string{
+"aop/List.txt",
+"aop/02code/json.txt",
+}
+//比如线上的程序的目录为game,"/Users/Jack/Downloads/QQreceiver/72bian_log/"下面的文件为我们线game下的程序文件内容
+cur,_:=os.Getwd()
+fmt.Println(cur)
+
+fori,f:=rangefileList{
+fileList[i]=filepath.Join(cur,f)
 }
 
 
-
-使用方法 一：
-
-func main() {
-    fileList := []string{
-        "/Users/Jack/Downloads/QQreceiver/72bian_log/anal.txt",
-        "/Users/Jack/Downloads/QQreceiver/72bian_log/config.json",
-        "/Users/Jack/Downloads/QQreceiver/72bian_log/xzwx_client/md5.txt",
-    }
-    //保留原来文件的结构
-    err := ZipFiles("/Users/Jack/Downloads/QQreceiver/test.zip", fileList, "","")
-    if err != nil {
-        fmt.Println(err)
-    }
+err:=ZipFiles("test.zip",fileList,filepath.Join(cur,"aop")+string(filepath.Separator),"")
+iferr!=nil{
+fmt.Println(err)
 }
-
-使用方法 二：
-替换成我们想要的结构
-
-func main() {
-    fileList := []string{
-        "/Users/Jack/Downloads/QQreceiver/72bian_log/anal.txt",
-        "/Users/Jack/Downloads/QQreceiver/72bian_log/config.json",
-        "/Users/Jack/Downloads/QQreceiver/72bian_log/xzwx_client/md5.txt",
-    }
-    //比如线上的程序的目录为game ,"/Users/Jack/Downloads/QQreceiver/72bian_log/"下面的文件为我们线game下的程序文件内容
-    err := ZipFiles("/Users/Jack/Downloads/QQreceiver/test.zip", fileList, "/Users/Jack/Downloads/QQreceiver/72bian_log","game/")
-    if err != nil {
-        fmt.Println(err)
-    }
 }
